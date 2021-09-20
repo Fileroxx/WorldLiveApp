9720b86f08bae7bbd43d76ee28ab60b989fcb04f
Igor Fernandes <igor_cesari@hotmail.com>
2021-09-20 16:33:43 -0300

    Erro RENDERIZAÇÃO - Corrigida

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,9 @@ import Head from 'next/head'
 import { Container } from 'react-bootstrap'
 import Navbar from '../src/components/Navbar'
 import Footer from '../src/components/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
 
@@ -11,13 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return(
   <>
-
-  
   <Component {...pageProps} />
-
-
-  
-  
   </>
   )
 }
